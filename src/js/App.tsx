6839ae347974e150c26d33815a84ba0769ede3b7
Peter Donker <peter@bring2mind.net>
2017-02-28 00:45:21 +0100

    Fix to ts system

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,5 +1,3 @@
-/// <reference path="../../typings/index.d.ts" />
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { UserManagementGrid } from "./Components/Users/UserManagement/UserManagementGrid";
